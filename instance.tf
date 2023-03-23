a2a7967e3b6a51da318ccf8e8f41ced7e461298c
praveen <praveenchary17@gmail.com>
2023-03-23 14:31:56 +0530

    updated main tf file

--- instance.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "webserver" {
-    ami = "ami-0557a15b87f6559cf"
-    instance_type = "t2.micro"
-    keyname = "webserver"
-    tags = { 
-        Name = "mywebserver"
-    }
-}
--- main.tf
+++ main.tf
@@ -19,3 +19,12 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+resource "aws_instance" "webserver" {
+    ami = "ami-0557a15b87f6559cf"
+    instance_type = "t2.micro"
+    keyname = "webserver"
+    tags = { 
+        Name = "mywebserver"
+    }
+}
+
