ec06615a28bd5a0bfaaa9cd7060927cc8b801e91
praveen <praveenchary17@gmail.com>
2023-03-23 14:12:34 +0530

    deleted s3 file and updated main file

--- main.tf
+++ main.tf
@@ -1,15 +1,6 @@
-resource "aws_instance" "webserver" {
-    ami = "ami-0557a15b87f6559cf"
-    instance_type = "t2.micro"
-    keyname = "webserver"
-    tags = { 
-        Name = "mywebserver"
-    }
-}
-
 resource "aws_s3_bucket" "terraform_state" {
   #S3 bucket name should be unique in a region
-  bucket = "terraform-state-demo3"
+  bucket = "terraformsaregudem"
   #Prevent accidental deletion
   lifecycle {
     prevent_destroy = true
