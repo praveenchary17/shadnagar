d547440cea089eb389c76589db47ba1c4e7a67fb
praveenchary <52570158+praveenchary17@users.noreply.github.com>
2023-03-27 14:47:06 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,6 +2,6 @@ provider "aws" {
         region = "us-east-1"
 }
 resource "aws_instance" "backend_demo" {
-    ami = "ami-05fa00d4c63e32376"
+    ami = "ami-0c9978668f8d55984"
     instance_type = "t2.micro"
 }
