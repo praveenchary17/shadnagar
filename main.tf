c70925c7c4c530dccf98815f186ed518cb60fc1e
praveenchary <52570158+praveenchary17@users.noreply.github.com>
2023-03-27 12:25:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "terraform_state" {
   #S3 bucket name should be unique in a region
-  bucket = "terraformsaregudem"
+  bucket = "terraform-state-demo3"
   #Prevent accidental deletion
   lifecycle {
     prevent_destroy = true
@@ -17,14 +17,5 @@ resource "aws_s3_bucket" "terraform_state" {
       }
     }
   }
-}
-
-resource "aws_instance" "webserver" {
-    ami = "ami-0557a15b87f6559cf"
-    instance_type = "t2.micro"
-    keyname = "webserver"
-    tags = { 
-        Name = "mywebserver"
-    }
 }
 
