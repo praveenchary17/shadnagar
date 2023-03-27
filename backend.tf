b5323dce1275e9f1f3d5cf2daef21caee83906a7
praveenchary <52570158+praveenchary17@users.noreply.github.com>
2023-03-27 14:46:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
         backend "s3" {
         bucket = "terraformsaregudem"
-        key="state/terraform.tfstate"
+        key="do-not-delete/state/terraform.tfstate"
         region="us-east-1"
+        dynamodb_table="saregudem-lock"
         }
 }
